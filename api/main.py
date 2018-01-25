adea963a0eb744457b5ca58592814ba0755fb3ef
Gavin Rehkemper <gavin@gavinr.com>
2018-01-25 15:56:29 -0600

    use json file

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 import json
 app = Flask(__name__)
 
-j = json.loads('{"one" : "1", "two" : "2", "three" : "3"}')
+j = json.load(open('options.json'))
 
 @app.route('/')
 def hello_world():
